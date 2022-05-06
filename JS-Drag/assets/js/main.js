835ebcc27da6cfb774cff7bd4742b3866be7560b
Mahammad Mustafayev <tu7ekdm78@aztu.local>
2022-05-06 13:11:46 +0400

    added

--- JS-Drag/assets/js/main.js
+++ JS-Drag/assets/js/main.js
@@ -1,36 +1,49 @@
-let fruit=document.getElementById("f");
-let vegetable=document.getElementById("v");
-let fruits=document.querySelector(".fruits");
-let vegetables=document.querySelector(".vegetables")
+let fruit = document.getElementById("f");
+let vegetable = document.getElementById("v");
+let fruits = document.querySelector(".fruits");
+let vegetables = document.querySelector(".vegetables")
+let mix = document.querySelector(".mix-basket");
 
-
-document.querySelectorAll(".fruit").forEach(item=>{
-    item.addEventListener("dragstart",function(e){
-        e.dataTransfer.setData("fId",this.id)
+document.querySelectorAll(".fruit").forEach(item => {
+    item.addEventListener("dragstart", function (e) {
+        e.dataTransfer.setData("fId", this.id)
     })
 })
 
-fruits.addEventListener("dragover",function(el){
+fruits.addEventListener("dragover", function (el) {
     el.preventDefault();
 })
 
-fruits.addEventListener("drop",function(elem){
-    let dara=elem.dataTransfer.getData("fId");
+fruits.addEventListener("drop", function (elem) {
+    let dara = elem.dataTransfer.getData("fId");
     this.appendChild(document.getElementById(dara));
 })
 
 
-document.querySelectorAll(".vegetable").forEach(val=>{
-    val.addEventListener("dragstart",function(e){
-        e.dataTransfer.setData("vId",this.id)
+document.querySelectorAll(".vegetable").forEach(val => {
+    val.addEventListener("dragstart", function (e) {
+        e.dataTransfer.setData("vId", this.id)
     })
 })
 
-vegetables.addEventListener("dragover",function(el){
+vegetables.addEventListener("dragover", function (el) {
     el.preventDefault();
 })
 
-vegetables.addEventListener("drop",function(elem){
-    let sada=elem.dataTransfer.getData("vId");
+vegetables.addEventListener("drop", function (elem) {
+    let sada = elem.dataTransfer.getData("vId");
     this.appendChild(document.getElementById(sada));
+})
+
+mix.addEventListener("dragstart", function (e) {
+    e.dataTransfer.setData("mixId", this.id);
+})
+
+mix.addEventListener("dragover", function (el) {
+    el.preventDefault();
+})
+
+mix.addEventListener("drop", function (elem) {
+    let basket = elem.dataTransfer.getData("mixId");
+    this.appendChild(document.getElementById(basket));
 })
